67b80efedc12c6b307ca27220bb6681b6393dee4
hhhynnn <2030206080@qq.com>
2022-03-13 23:53:39 +0800

    加入 ListView
    正在找如何实现每一行展示一个新闻

--- src/moduels/gui/MainWindow.py
+++ src/moduels/gui/MainWindow.py
@@ -1,19 +1,23 @@
-import os, sys, time
+import os
+import sys
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))  # 更改工作目录，指向正确的当前文件夹
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))  # 将当前目录导入 python 寻找 package 和 moduel 的变量
 
 from PySide6.QtGui import *
 from PySide6.QtCore import *
-
 from PySide6.QtWidgets import *
-from Combo_SearchMode import Combo_SearchMode
+
+from ListWidget_Result import ListWidget_Result
 
 
 class MainWindow(QMainWindow):
 
     def __init__(self):
         super(MainWindow, self).__init__()
+        ########################################
+        # 控件
+        ########################################
         # 总布局
         self.central_widget = QWidget()  # 主页面必须有 Widget, 否则不会显示任何东西
         self.central_layout = QVBoxLayout()
@@ -23,14 +27,25 @@ class MainWindow(QMainWindow):
         self.search_layout = QHBoxLayout()
         self.input_text = QLineEdit()
         self.search_button = QPushButton("◎")
-        self.mode_menu = Combo_SearchMode()
+        self.mode_menu = QComboBox()
 
         # 结果框
         self.result_box = QGroupBox("结果框")
         self.result_layout = QHBoxLayout()
-        self.result_scroll_area = QScrollArea()
+        self.result_list = ListWidget_Result()
 
+        ########################################
+        # 变量
+        ########################################
+        self.MODES = ['News', 'Image']
+        self.keywords = ''
+        self.mode = self.MODES[0]
+
+        ########################################
+        # 初始化
+        ########################################
         self.initLayout()
+        self.initSlot()
 
     def initLayout(self):
         # 整体框架
@@ -48,7 +63,31 @@ class MainWindow(QMainWindow):
         self.search_layout.addWidget(self.input_text)
         self.search_layout.addWidget(self.search_button)
         self.search_layout.addWidget(self.mode_menu)
+        self.search_button.setMaximumWidth(30)
+        self.mode_menu.setMaximumWidth(100)
 
         # 展示框
         self.result_box.setLayout(self.result_layout)
-        self.result_layout.addWidget(self.result_scroll_area)
+        self.result_layout.addWidget(self.result_list)
+
+        # 搜索选项
+        self.mode_menu.addItems(self.MODES)
+
+    def initSlot(self):
+        self.search_button.clicked.connect(self.search)
+        self.input_text.textChanged.connect(self.save_input)
+        self.mode_menu.currentTextChanged.connect(self.save_mode)
+
+    def initValue(self):
+        pass
+
+    def search(self):
+        print(f'search: {self.keywords}, mode={self.mode}')
+
+    def save_input(self, s):
+        self.keywords = s
+        print(s)
+
+    def save_mode(self, s):
+        self.mode = s
+        print(s)
